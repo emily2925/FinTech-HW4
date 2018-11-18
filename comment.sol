0728d1a2d39ee3254248d1574314cc7432bbe47c
emily2925 <30750085+emily2925@users.noreply.github.com>
2018-11-18 22:00:48 +0800

    Update comment.sol

--- comment.sol
+++ comment.sol
@@ -1,5 +1,7 @@
 /*
-comment here
+purchaseCD這個函數可執行購買定存，首先以require規定msg.sender之定存帳戶為零，表示此人
+原本沒有定存帳戶。然後再以require要求其輸入的定存期數要大於零。接著把msg.sender的存款
+金額和設定定存期數存到cd裡。再用emit告知外界購買定存的完成。
 */
 
 // 購買定存
@@ -19,7 +21,8 @@ function purchaseCD(uint256 periods) public payable {
 
 
 /*
-comment here
+terminateCD這個函數用來執行提前終止定存合約。首先以require規定msg.sender的帳戶餘額不為零，否則
+會警告該定存帳戶不存在。
 */
 
 // 提前中止定存合約
@@ -45,7 +48,8 @@ function terminateCD(uint256 periods) public {
 }
 
 /*
-comment here
+completeCD用以執行定存期滿的結算。同樣以require要求定存帳戶餘額不為零，然後分別將該定存戶的
+本金、期數、本金加利息存到value、periods、withProfit裡。再以emit告知外界定存期滿。
 */
 
 // 定存期滿
@@ -70,7 +74,7 @@ function completeCD() public {
 }
 
 /*
-comment here
+calcCD利用value(本金)和periods(期數)計算總共的本金加利息為多少
 */
 
 // 計算回饋定存金額
@@ -79,7 +83,7 @@ function calcCD(uint256 value, uint256 periods) private pure returns (uint256) {
 }
 
 /*
-comment here
+getCD用以了解msg.sender之本金和定存期數
 */
 
 // 取得定存狀態
